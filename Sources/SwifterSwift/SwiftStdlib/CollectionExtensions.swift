87ab518689d77bbb95720559087b8f18b20efce3
Guy Kogus <guy.kogus@gmail.com>
2020-06-09 11:41:21 +0200

    Fix indentation

--- Sources/SwifterSwift/SwiftStdlib/CollectionExtensions.swift
+++ Sources/SwifterSwift/SwiftStdlib/CollectionExtensions.swift
@@ -86,8 +86,8 @@ public extension Collection {
         var start = startIndex
         while case let end = index(start, offsetBy: slice, limitedBy: endIndex) ?? endIndex,
             start != end {
-             try body(Array(self[start..<end]))
-             start = end
+                try body(Array(self[start..<end]))
+                start = end
         }
     }
 
